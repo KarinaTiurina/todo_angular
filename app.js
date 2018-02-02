3404b3a4fe08e0a1404fe452e36b2778f926d1fd
Karina Tiurina <rktiurina@gmail.com>
2018-02-02 09:16:37 +0300

    add ng-show

--- app.js
+++ app.js
@@ -2,18 +2,10 @@ var myApp = angular.module('myApp', []);
 
 
 myApp.controller('mainController', ['$scope', '$http', function($scope, $http) {
-	// $scope.months = [
-	//     { name: "Февраль"},
-	//     { name: "Март"}
-	// ];
-
 	$scope.months = null;
     $http.get('json/timetable.json').success(function(data) {
         $scope.months = data.months;
     });
-
-    console.log($scope.months);
- 
 }]);
 
 
