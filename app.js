cfcda0180ef35e637ba865f9d7c9fdb12819e759
Karina Tiurina <rktiurina@gmail.com>
2018-02-02 09:27:30 +0300

    code style corrections

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var myApp = angular.module('myApp', []);
 
 
 myApp.controller('mainController', ['$scope', '$http', function($scope, $http) {
-	$scope.months = null;
+    $scope.months = null;
     $http.get('json/timetable.json').success(function(data) {
         $scope.months = data.months;
     });
