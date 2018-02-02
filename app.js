f8e90c8e9bc5db06552643e34cc901455080cc9c
Karina Tiurina <rktiurina@gmail.com>
2018-02-02 09:04:17 +0300

    ng-repeat show data from json file

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 var myApp = angular.module('myApp', []);
 
 
-myApp.controller('mainController', [function() {
+myApp.controller('mainController', ['$scope', '$http', function($scope, $http) {
+	// $scope.months = [
+	//     { name: "Февраль"},
+	//     { name: "Март"}
+	// ];
+
+	$scope.months = null;
+    $http.get('json/timetable.json').success(function(data) {
+        $scope.months = data.months;
+    });
+
+    console.log($scope.months);
  
 }]);
 
