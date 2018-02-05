1719cb13bd55f596ff29886a42180281f9620848
Karina Tiurina <rktiurina@gmail.com>
2018-02-05 10:53:37 +0300

    directives

--- app.js
+++ app.js
@@ -8,4 +8,33 @@ myApp.controller('mainController', ['$scope', '$http', function($scope, $http) {
     });
 }]);
 
+myApp.directive("taskDiv", function(){
+  return {
+    templateUrl: 'directives/taskDiv.html',
+    replace: true,
+    scope: {
+      taskObject: "="
+    }
+  }
+});
 
+myApp.directive("weekDiv", function(){
+  return {
+    templateUrl: 'directives/weekDiv.html',
+    replace: true,
+    scope: {
+      weeksObject: "=",
+      index: '@'
+    }
+  }
+});
+
+myApp.directive("monthDiv", function(){
+  return {
+    templateUrl: 'directives/monthDiv.html',
+    replace: true,
+    scope: {
+      monthObject: "="
+    }
+  }
+});
