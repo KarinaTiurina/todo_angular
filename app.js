140a766ee1df983144e5c2b47bbf7532de1cf05e
Karina Tiurina <rktiurina@gmail.com>
2018-02-06 11:59:47 +0300

    add service dbService

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$http', function($scope, $http) {
-  setItems();
-
-  $scope.months = [];
-
-  for(var key in localStorage) {
-    var month = JSON.parse(localStorage.getItem(key));
-    $scope.months.push(month);
-  }
-  $scope.months = $scope.months.filter(function(e){return e});
-
+myApp.controller('mainController', ['$scope', '$http', 'dbService', function($scope, $http, dbService) {
+  dbService.setItems();
+  $scope.months = dbService.getMonths();
 
   console.log($scope.months);
 
@@ -45,4 +37,68 @@ myApp.directive("monthDiv", function(){
       monthObject: "="
     }
   }
+});
+
+myApp.service('dbService', function(){
+  this.setItems = function() {
+    var feb = { "name": "Февраль",
+      "weeks": [
+            { "week": [
+                { "task": "задание №1" },
+                { "task": "задание №2" }
+              ]
+            },
+            { "week": [
+                { "task": "задание №1" },
+                { "task": "задание №2" },
+                { "task": "задание №3" },
+                { "task": "задание №4" }
+              ]
+            },
+            { "week": [
+                { "task": "задание №1" },
+                { "task": "задание №2" },
+                { "task": "задание №4" }
+              ]
+            }
+      ]
+    };
+
+    localStorage.setItem("feb", JSON.stringify(feb));
+
+    var march = { "name": "Март",
+      "weeks": [
+            { "week": [
+                { "task": "задание №1" },
+                { "task": "задание №2" }
+              ]
+            },
+            { "week": [
+                { "task": "задание №1" },
+                { "task": "задание №2" },
+                { "task": "задание №3" }
+              ]
+            },
+            { "week": [
+                { "task": "задание №1" },
+                { "task": "задание №2" },
+                { "task": "задание №4" }
+              ]
+            }
+      ]
+    };
+
+    localStorage.setItem("march", JSON.stringify(march));
+  };
+
+  this.getMonths = function() {
+    var months = [];
+
+    for(var key in localStorage) {
+      var month = JSON.parse(localStorage.getItem(key));
+      months.push(month);
+    }
+
+    return months.filter(function(e){return e});
+  };
 });
